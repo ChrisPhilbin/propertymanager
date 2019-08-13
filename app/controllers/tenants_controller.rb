d41cefadb40f76fd8325fdac04e3c705bc72d08d
Chris Philbin <chris@chrisphilbin.net>
2019-08-13 19:03:10 -0400

    Getting customer tenant#all route to work and implementing handler to render json in controller

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -41,6 +41,10 @@ class TenantsController < ApplicationController
 	def all
 		@landlord = Landlord.find(params[:id])
 		@tenants = Tenant.where(landlord_id: params[:id])
+		respond_to do |format|
+			format.json { render json: [@landlord, @tenants], status: 201}
+			format.html { render 'all' }
+		end
 	end
 
 	private
